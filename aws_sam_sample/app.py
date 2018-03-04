8e93e7895d58dcada74023400a75fdba87c4dbac
tadashi-aikawa <syou.maman@gmail.com>
2018-03-05 08:30:35 +0900

    Use multibyte name for test

--- aws_sam_sample/app.py
+++ aws_sam_sample/app.py
@@ -4,9 +4,9 @@ from aws_sam_sample.libs.models import Event
 
 def main(event, context):
     e: Event = Event(**event)
-
     member: Member = find_member(e.id)
     return {
         'id': member.id,
         'name': member.name
     }
+
--- tests/test_dao.py
+++ tests/test_dao.py
@@ -1,4 +1,4 @@
-from aws_sam_sample.libs.dao import find_human, Human, Base
+from aws_sam_sample.libs.dao import find_member, Member, Base
 from aws_sam_sample.libs.session import Session, engine
 
 
@@ -6,11 +6,12 @@ def test_hoge():
     Base.metadata.create_all(bind=engine)
     session = Session()
     session.add_all([
-        Human(id=1, name='tadashi-aikawa'),
-        Human(id=2, name='Ichiro')
+        Member(id=1, name='イチロー', age=40),
+        Member(id=2, name='ジロー', age=30)
     ])
     session.commit()
 
-    actual: Human = find_human(1)
-    assert actual.name == 'tadashi-aikawa'
+    actual: Member = find_member(1)
+    assert actual.name == 'イチロー'
+    assert actual.age == 40
 
