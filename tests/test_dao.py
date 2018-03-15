df953a9dcd3e1afaba61457ec525389e23e6c9d6
tadashi-aikawa <syou.maman@gmail.com>
2018-03-15 19:31:00 +0900

    :hammer: Use fixture for test

--- tests/test_dao.py
+++ tests/test_dao.py
@@ -1,33 +1,30 @@
+import pytest
+
 from aws_sam_sample.dao import Base, Member, find_member
 from aws_sam_sample.session import Session, engine
 
 
+@pytest.fixture(autouse=True)
+def initdb():
+    Base.metadata.create_all(bind=engine)
+    session = Session()
+    session.add_all([
+        Member(id='0001', name='イチロー', age=40),
+        Member(id='0002', name='ジロー', age=30)
+    ])
+    session.commit()
+
+    yield
+
+    session.query(Member).delete()
+
+
 class TestFindMember():
     def test_found(self):
-        Base.metadata.create_all(bind=engine)
-        session = Session()
-        session.add_all([
-            Member(id='0001', name='イチロー', age=40),
-            Member(id='0002', name='ジロー', age=30)
-        ])
-        session.commit()
-
         actual: Member = find_member('0001')
         assert actual.name == 'イチロー'
         assert actual.age == 40
 
-        session.query(Member).delete()
-
     def test_not_found(self):
-        Base.metadata.create_all(bind=engine)
-        session = Session()
-        session.add_all([
-            Member(id='0001', name='イチロー', age=40),
-            Member(id='0002', name='ジロー', age=30)
-        ])
-        session.commit()
-
         actual: Member = find_member('9999')
         assert actual is None
-
-        session.query(Member).delete()
